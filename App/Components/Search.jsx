9aa1ec74b639955f115bcb47dd6d6d368b9a87d0
doandung <doantrungdung2001@gmail.com>
2024-04-07 15:11:10 +0700

    add search grow vegetables

--- App/Components/Search.jsx
+++ App/Components/Search.jsx
@@ -7,37 +7,50 @@ import {
   TouchableOpacity,
 } from "react-native";
 import React, { useState } from "react";
-import { COLORS } from "../Constants";
+import { COLORS, SIZES, FONT } from "../Constants";
 import { FontAwesome } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { Entypo } from "@expo/vector-icons";
 
-const typeSearch = [
-  "Nearest",
-  "All",
-  "Bac",
-  "Trung",
-  "Nam",
-];
+const typeSearch = ["Nearest", "All", "Bac", "Trung", "Nam"];
 const Search = () => {
   const [activeJobSearch, setActiveJobSearch] = useState("All");
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <View style={styles.searchBarContainer}>
-        <TextInput placeholder="Nhập tên trang trại" style={styles.textInput} />
+        <TextInput
+          placeholder="Nhập tên trang trại"
+          value=""
+          onChange={() => {}}
+          style={styles.textInput}
+        />
         <FontAwesome
           name="search"
           size={24}
           color={COLORS.primary}
-          style={styles.searchbtn}
+          style={styles.searchBtn}
         />
       </View>
       <View style={styles.tabsContainer}>
+        <Entypo name="bookmarks" size={30} color="white" />
         <FlatList
           data={typeSearch}
           renderItem={({ item }) => (
-            <TouchableOpacity style={styles.tab(activeJobSearch, item)}>
-              <Text>{item}</Text>
+            <TouchableOpacity
+              style={styles.tab(activeJobSearch, item)}
+              onPress={() => {
+                setActiveJobSearch(item);
+                // navigation.push(`/search/${item}`);
+                console.log(item);
+              }}
+            >
+              <Text style={styles.tabTexts(activeJobSearch, item)}>{item}</Text>
             </TouchableOpacity>
           )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
         />
       </View>
     </View>
@@ -63,10 +76,27 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     width: "85%",
   },
-  searchbtn: {
+  searchBtn: {
     backgroundColor: COLORS.white,
     padding: 10,
-    borderRadius: 8,
+    borderRadius: 10,
   },
-  tabsContainer: {},
+  tabsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    marginTop: SIZES.medium,
+    gap: 20,
+  },
+  tab: (activeJobType, item) => ({
+    paddingVertical: SIZES.small / 2,
+    paddingHorizontal: SIZES.small,
+    borderRadius: SIZES.medium,
+    borderWidth: 1,
+    borderColor: activeJobType === item ? COLORS.secondary : COLORS.white,
+  }),
+  tabTexts: (activeJobType, item) => ({
+    // fontFamily: FONT.medium,
+    color: activeJobType === item ? COLORS.secondary : COLORS.white,
+  }),
 });
