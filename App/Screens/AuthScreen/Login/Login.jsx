b584c41e0e16ad1a8cb0959fca7bcf2140716e10
doandung <doantrungdung2001@gmail.com>
2024-04-24 19:42:31 +0700

    update css in Auth Screen

--- App/Screens/AuthScreen/Login/Login.jsx
+++ App/Screens/AuthScreen/Login/Login.jsx
@@ -12,6 +12,7 @@ import { useNavigation } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
 import AUTH from "../../../Services/AuthService";
 import UserInfoAsyncStorage from "../../../Utils/UserInfoAsyncStorage";
+import styles from "./Login.Style";
 
 const Login = () => {
   const navigation = useNavigation();
@@ -129,86 +130,3 @@ const Login = () => {
   );
 };
 export default Login;
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 60,
-    padding: 20,
-  },
-  header: {
-    alignItems: "center",
-  },
-  headerText: {
-    fontSize: 40,
-    color: COLORS.primary,
-    fontFamily: "RobotoCondensed-Bold",
-  },
-  content: {
-    marginVertical: 30,
-  },
-  textInputEmail: {
-    fontFamily: "Roboto-Medium",
-    fontSize: 20,
-    padding: 20,
-    backgroundColor: COLORS.white,
-    borderRadius: 10,
-    marginVertical: 10,
-  },
-  password: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  textInputPassword: {
-    fontFamily: "Roboto-Medium",
-    fontSize: 20,
-    padding: 20,
-    backgroundColor: COLORS.white,
-    borderRadius: 10,
-    marginVertical: 10,
-    flex: 1,
-  },
-  displayPassword: {
-    position: "absolute",
-    right: 20,
-  },
-  forgetPassword: {
-    fontFamily: "Roboto-BoldItalic",
-    fontSize: 18,
-    alignSelf: "flex-end",
-    color: COLORS.primary,
-  },
-  btnLogin: {
-    padding: 20,
-    backgroundColor: COLORS.primary,
-    marginVertical: 30,
-    borderRadius: 10,
-    shadowColor: COLORS.green,
-    shadowOffset: { width: 0, height: 10 },
-    shadowOpacity: 0.3,
-    shadowRadius: 10,
-  },
-  textBtnLogin: {
-    fontFamily: "RobotoCondensed-Bold",
-    color: COLORS.white,
-    textAlign: "center",
-    fontSize: 25,
-  },
-  btnRegister: {
-    padding: 10,
-    backgroundColor: COLORS.white,
-    borderRadius: 10,
-    shadowColor: COLORS.green,
-    shadowOffset: { width: 0, height: 10 },
-    shadowOpacity: 0.3,
-    shadowRadius: 10,
-  },
-  textBtnRegister: {
-    fontFamily: "RobotoCondensed-Bold",
-    color: COLORS.black,
-    textAlign: "center",
-    fontSize: 19,
-  },
-  alertLogin: {
-    color: "red",
-    fontSize: 17,
-  },
-});
--- App/Screens/AuthScreen/Register/Register.jsx
+++ App/Screens/AuthScreen/Register/Register.jsx
@@ -10,6 +10,8 @@ import React, { useState } from "react";
 import { COLORS } from "../../../Constants";
 import { useNavigation } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
+import styles from "./Register.Style";
+
 const Register = () => {
   const navigation = useNavigation();
   const [selectDisplayPassword, setSelectDisplayPassowrd] = useState(false);
@@ -126,76 +128,4 @@ const Register = () => {
 
 export default Register;
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 60,
-    padding: 20,
-  },
-  header: {
-    alignItems: "center",
-  },
-  textHeader: {
-    fontSize: 40,
-    color: COLORS.primary,
-    fontFamily: "RobotoCondensed-Bold",
-  },
-  content: {
-    marginVertical: 30,
-  },
-  textInputEmail: {
-    fontFamily: "Roboto-Medium",
-    fontSize: 20,
-    padding: 20,
-    backgroundColor: COLORS.white,
-    borderRadius: 10,
-    marginVertical: 10,
-  },
-  password: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  textInputPassword: {
-    fontFamily: "Roboto-Medium",
-    fontSize: 20,
-    padding: 20,
-    backgroundColor: COLORS.white,
-    borderRadius: 10,
-    marginVertical: 10,
-    flex: 1,
-  },
-  dispalyPassword: {
-    position: "absolute",
-    right: 20,
-  },
-  btnRegister: {
-    padding: 20,
-    backgroundColor: COLORS.primary,
-    marginVertical: 30,
-    borderRadius: 10,
-    shadowColor: COLORS.green,
-    shadowOffset: { width: 0, height: 10 },
-    shadowOpacity: 0.3,
-    shadowRadius: 10,
-  },
-  textBtnRegister: {
-    fontFamily: "RobotoCondensed-Bold",
-    color: COLORS.white,
-    textAlign: "center",
-    fontSize: 25,
-  },
-  btnLogin: {
-    padding: 10,
-    backgroundColor: COLORS.white,
-    borderRadius: 10,
-    shadowColor: COLORS.green,
-    shadowOffset: { width: 0, height: 10 },
-    shadowOpacity: 0.3,
-    shadowRadius: 10,
-  },
-  textBtnLogin: {
-    fontFamily: "RobotoCondensed-Bold",
-    color: COLORS.black,
-    textAlign: "center",
-    fontSize: 19,
-  },
-});
+
