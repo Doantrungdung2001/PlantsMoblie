8379f45ab2b6f9b1326fa4f059608a103471ec76
doandung <doantrungdung2001@gmail.com>
2024-04-26 13:57:41 +0700

    update css heading

--- App/Components/Heading/Heading.Styles.js
+++ App/Components/Heading/Heading.Styles.js
@@ -7,3 +7,5 @@ const styles = StyleSheet.create({
     marginLeft: 15,
   },
 });
+
+export default styles;
--- App/Components/Heading/Heading.jsx
+++ App/Components/Heading/Heading.jsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 import React from "react";
-import styles from "../HeaderComponents/Header.Styles";
+import styles from "./Heading.Styles";
 
 const Heading = ({ text }) => {
   return (
