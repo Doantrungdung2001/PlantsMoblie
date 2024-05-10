d33b8934fb0bd34578cd1635ae985ce8645b1c2b
doandung <doantrungdung2001@gmail.com>
2024-05-10 23:02:53 +0700

    add button components and input

---
+++ App/Components/InputField/InputField.jsx
@@ -0,0 +1,40 @@
+import { Text, View } from "react-native";
+import React from "react";
+import styles from "./InputField.Styles";
+const InputField = ({
+  label,
+  icon,
+  inputType,
+  keyboardType,
+  fieldButtonLabel,
+  fieldButtonFunction,
+}) => {
+  return (
+    <View
+      style={styles.container}
+    >
+      {icon}
+      {inputType == "password" ? (
+        <TextInput
+          placeholder={label}
+          keyboardType={keyboardType}
+          style={{ flex: 1, paddingVertical: 0 }}
+          secureTextEntry={true}
+        />
+      ) : (
+        <TextInput
+          placeholder={label}
+          keyboardType={keyboardType}
+          style={{ flex: 1, paddingVertical: 0 }}
+        />
+      )}
+      <TouchableOpacity onPress={fieldButtonFunction}>
+        <Text style={styles.labelBtn}>
+          {fieldButtonLabel}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default InputField;
